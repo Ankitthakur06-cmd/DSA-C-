14c25a8279d3e32a2c04c9005e30f9091a6739fc
Kumar Ankit <thakurkumar659@gmail.com>
2026-07-16 23:07:49 +0530

    Updated practice files

--- Referencevariable.cpp
+++ Referencevariable.cpp
@@ -13,13 +13,33 @@ int main(){
 
     int n;
     cin>>n;
-    int *arr= new int[n];
+    // int *arr= new int[n];
+
+    // for(int i = 0;i<n;i++){
+    //     cin>>arr[i];
+    // }
+    // int ans = getsum(arr,n);
+    // cout<<"Ans is "<<ans;
+
+    int **arr = new int*[n];
+    for(int i = 0;i<n;i++){
+        arr[i] = new int[n];
+    }
+    // creation of 2d array done
 
     for(int i = 0;i<n;i++){
-        cin>>arr[i];
+        for(int j = 0;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }cout<<endl;
+    // Input done
+
+    // Output
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<n;j++){
+            cout<<arr[i][j]<<" ";
+        }cout<<endl;
     }
-    int ans = getsum(arr,n);
-    cout<<"Ans is "<<ans;
     
     return 0;
 }
